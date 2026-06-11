663a95ddf6046f7d887a80291d9cdba8374be8f4
Ajay <ajaykumarofficialh20@gmail.com>
2026-06-11 19:00:24 +0530

    Update Bot.py
    Replaced the bot token with a placeholder for security reasons.

--- Bot.py
+++ Bot.py
@@ -10,7 +10,7 @@ from telegram.error import TelegramError
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "8829579785:AAH79xvOakNnmZFrouB8S1pchFFyj-ho4GY"
+bot token ()
 
 warnings = {}
 
